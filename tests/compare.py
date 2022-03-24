9a01608f2c39e49a90dbfa406382fb7111bc7945
Daniil Lyubaev <lyubaevda@gmail.com>
2022-03-24 14:15:07 +0300

    Fix tests with bash script

--- tests/compare.py
+++ tests/compare.py
@@ -1,42 +1,22 @@
-from os import listdir, popen, getcwd
-from os.path import isfile, join
+from sys import argv
 import subprocess
 
-def get_ldd_repl_out(filename):
-    return subprocess.check_output([getcwd() + '/ldd-repl', filename])
+def main(orig_file, repl_file):
+    out_orig = set()
+    for p_str in open(orig_file, 'r').read().split('\n')[1:-1]:
+        out_orig.add(p_str.strip().split()[0].split('/')[-1])
 
-def get_ldd_orig_out(filename):
-    return subprocess.check_output(['ldd', filename])
+    out_repl = set()
+    for p_str in open(repl_file, 'r').read().split('\n')[:-1]:
+        p_list = p_str.strip().split()
+        out_repl.add(p_list[0])
 
-def main():
-    bin_path = '/bin'
-    print('Bin path:', bin_path)
-    bin_files = [(bin_path + '/' + f) for f in listdir(bin_path) if isfile(join(bin_path, f))]
-    print('Bin files count:', len(bin_files))
-
-    test_files_counts = min(len(bin_files), 3)
-
-    for filename in bin_files[:test_files_counts]:
-        print('Current file:', filename)
-        try:
-            out_orig = set()
-            for p_str in get_ldd_orig_out(filename).decode('ISO-8859-1').split('\n')[1:-1]:
-                out_orig.add(p_str.strip().split()[0].split('/')[-1])
-
-            out_repl = set()
-            for p_str in get_ldd_repl_out(filename).decode('ISO-8859-1').split('\n')[:-1]:
-                p_list = p_str.strip().split()
-                out_repl.add(p_list[0])
-
-        #     if out_orig != out_repl:
-        #         print('Not equal on', filename)
-        #         print('Original:', out_orig)
-        #         print('Replica:', out_repl)
-        #         exit(1) 
-
-        except subprocess.CalledProcessError:
-            continue
+    if out_orig != out_repl:
+        print('Not equal!')
+        print('Original:', out_orig)
+        print('Replica:', out_repl)
+        exit(1)
 
 
 if __name__ == '__main__':
-    main()
+    main(argv[1], argv[2])
---
+++ tests/get_out_and_compare.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+i=0
+for filename in $(find /usr/bin -maxdepth 1 -type f -executable); do
+    echo "Current file: $filename"
+    ../ldd-repl $filename > repl.out
+    ldd $filename > orig.out
+    python3 compare.py orig.out repl.out
+    if [[ $? == 1 ]]; then
+        exit 1
+    fi
+    
+    i=$(($i+1))
+    if [[ $i == 10 ]]; then
+        break
+    fi
+done
+
+rm orig.out repl.out
