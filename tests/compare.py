e1801bac179d5ef39dc950fefc14b9df171d31f7
Daniil Lyubaev <lyubaevda@gmail.com>
2022-03-24 13:28:58 +0300

    Testing tests

--- tests/compare.py
+++ tests/compare.py
@@ -16,23 +16,23 @@ def main():
 
     test_files_counts = min(len(bin_files), 5)
 
-    # for filename in bin_files[:test_files_counts]:
-    #     print('Current file:', filename)
-    #     try:
-    #         out_orig = set()
-    #         for p_str in get_ldd_orig_out(filename).decode('ISO-8859-1').split('\n')[1:-1]:
-    #             out_orig.add(p_str.strip().split()[0].split('/')[-1])
+    for filename in bin_files[:test_files_counts]:
+        print('Current file:', filename)
+        # try:
+        #     out_orig = set()
+        #     for p_str in get_ldd_orig_out(filename).decode('ISO-8859-1').split('\n')[1:-1]:
+        #         out_orig.add(p_str.strip().split()[0].split('/')[-1])
 
-    #         out_repl = set()
-    #         for p_str in get_ldd_repl_out(filename).decode('ISO-8859-1').split('\n')[:-1]:
-    #             p_list = p_str.strip().split()
-    #             out_repl.add(p_list[0])
+        #     out_repl = set()
+        #     for p_str in get_ldd_repl_out(filename).decode('ISO-8859-1').split('\n')[:-1]:
+        #         p_list = p_str.strip().split()
+        #         out_repl.add(p_list[0])
 
-    #         if out_orig != out_repl:
-    #             print('Not equal on', filename)
-    #             print('Original:', out_orig)
-    #             print('Replica:', out_repl)
-    #             exit(1) 
+        #     if out_orig != out_repl:
+        #         print('Not equal on', filename)
+        #         print('Original:', out_orig)
+        #         print('Replica:', out_repl)
+        #         exit(1) 
 
     #     except subprocess.CalledProcessError:
     #         continue
