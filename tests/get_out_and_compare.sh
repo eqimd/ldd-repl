2bc3747e4c21710ee06c7ba164766d6fd0be6b3c
Daniil Lyubaev <lyubaevda@gmail.com>
2022-03-24 14:18:31 +0300

    break repl out

--- tests/get_out_and_compare.sh
+++ tests/get_out_and_compare.sh
@@ -3,7 +3,7 @@
 i=0
 for filename in $(find /usr/bin -maxdepth 1 -type f -executable); do
     echo "Current file: $filename"
-    ../ldd-repl $filename > repl.out
+    # ../ldd-repl $filename > repl.out
     ldd $filename > orig.out
     # python3 compare.py orig.out repl.out
     # if [[ $? == 1 ]]; then
